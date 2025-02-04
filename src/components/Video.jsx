65a8578836594c8c08157ebfc6adfc1191c19543
Masum Billah <bograhost@gmail.com>
2025-02-04 15:13:59 +0600

    4

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,27 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-export class Video extends Component {
-  render() {
-    return (
-        <section className="tr_video text-center wow fadeInUp" style={{ backgroundImage: `url(assets/img/bg/video.jpg)` }}>
-            <Link to="https://www.youtube.com/watch?v=5s8fs_j2xlY" className="video_btn"><i className="ph ph-play"></i></Link>
-            <div className="trv_title"><h1>Travel</h1></div>
-        </section>
-    )
-  }
+import React, { useState } from 'react';
+import ModalVideo from 'react-modal-video';
+
+const Video = () => {
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <React.Fragment>
+      <ModalVideo
+          channel="youtube"
+          youtube={{ mute: 0, autoplay: 0 }}
+          isOpen={isOpen}
+          videoId="5s8fs_j2xlY"
+          onClose={() => setOpen(false)} 
+        />
+      <section className="tr_video text-center wow fadeInUp" style={{ backgroundImage: `url(assets/img/bg/video.jpg)` }}> 
+      
+        <div className="trv_title"><h1>Travel</h1></div>
+        <button className="video_btn text-center" onClick={() => setOpen(true)}>
+          <i className="ph ph-play"></i>
+        </button>     
+      </section>
+    </React.Fragment> 
+  )
 }
+
 
 export default Video
